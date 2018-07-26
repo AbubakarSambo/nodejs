b7e4e06480c5d714265cc343957290e6393ce253
Abubakar sambo <abubakar.sambo@mail.mcgill.ca>
2018-07-26 12:23:40 +0100

    refactored code

--- app/index.js
+++ app/index.js
@@ -6,79 +6,49 @@
 // Dependencies
 var http = require('http');
 var url = require('url');
-var StringDecoder = require('string_decoder').StringDecoder;
 
- // Configure the server to respond to all requests with a string
-var server = http.createServer(function(req,res){
+var server = http.createServer(function (req, res) {
 
-  // Parse the url
   var parsedUrl = url.parse(req.url, true);
 
-  // Get the path
   var path = parsedUrl.pathname;
   var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-  // Get the query string as an object
-  var queryStringObject = parsedUrl.query;
-
-  // Get the HTTP method
   var method = req.method.toLowerCase();
 
-  //Get the headers as an object
-  var headers = req.headers;
+  var chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
-  // Get the payload,if any
-  var decoder = new StringDecoder('utf-8');
-  var buffer = '';
-  req.on('data', function(data) {
-      buffer += decoder.write(data);
-  });
-  req.on('end', function() {
-      buffer += decoder.end();
 
-      var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+  chosenHandler(method, function (statusCode, payload) {
+    statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+    payload = typeof (payload) == 'object' ? payload : {};
+    var payloadString = JSON.stringify(payload);
 
-      var data = {
-        'trimmedPath' : trimmedPath,
-        'queryStringObject' : queryStringObject,
-        'method' : method,
-        'headers' : headers,
-        'payload' : buffer
-      };
-
-      
-      chosenHandler(data,function(statusCode,payload){
-        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-        payload = typeof(payload) == 'object'? payload : {};
-        var payloadString = JSON.stringify(payload);
-
-        res.setHeader('Content-Type', 'application/json');
-        res.writeHead(statusCode);
-        res.end(payloadString);
-
-      });
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(statusCode);
+    res.end(payloadString);
 
   });
+
 });
 
-server.listen(3000,function(){
+server.listen(3000, function () {
   console.log('The server is up and running now');
 });
 
 var handlers = {};
 
-handlers.welcome = function(data,callback){
-  if(data.method === 'post'){
-    callback(200,{'message':'Welcome to the API son!'});
+handlers.welcome = function (method, callback) {
+  if (method === 'post') {
+    callback(200, { 'message': 'Welcome to the API son!' });
   }
   else {
-    callback(404,{'error':'Not found'});
+    callback(404, { 'error': 'Not found' });
 
   }
 };
-handlers.notFound = function(data,callback){
+handlers.notFound = function (data, callback) {
   callback(407);
 };
 var router = {
-  'welcome' : handlers.welcome
+  'welcome': handlers.welcome
 };
